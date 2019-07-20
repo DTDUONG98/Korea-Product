a38dafbef283557ad80455b5e02740ba24d5b96c
DTDUONG98 <dotienduongptit@gmail.com>
2019-07-20 22:46:30 +0700

    update code

---
+++ frontend/src/View/Login/Index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from '../../../node_modules/react'
+
+
+class index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div >
+              <p>
+                  GGGGG
+              </p>
+            </div>
+        );
+    }
+}
+
+export default index;
