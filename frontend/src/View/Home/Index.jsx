eb49ab67f2211270282cfca79834a582e0155a95
dotienduong <do.tien.duong@mqsolutions.com.vn>
2019-08-16 15:19:25 +0700

    update home page

--- frontend/src/View/Home/Index.jsx
+++ frontend/src/View/Home/Index.jsx
@@ -7,13 +7,13 @@ import InputBase from '@material-ui/core/InputBase'
 import { fade } from '@material-ui/core/styles'
 import Avatar from '@material-ui/core/Avatar';
 import img from '../../../src/PTIT.png'
-import { Fade } from 'react-slideshow-image';
-import { Grid, Paper } from '@material-ui/core';
+import { Grid, Paper, Container } from '@material-ui/core';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import { Zoom } from 'react-slideshow-image';
 
 const style = theme => ({
     root: {
@@ -27,13 +27,21 @@ const style = theme => ({
     header: {
         textAlign: 'center',
         width: '100%',
-        height: '150px'
+        height: 'auto'
     },
     body: {
         textAlign: 'center',
         width: '100%',
         height: '150px',
         marginTop: '450px',
+    },
+    footer: {
+        background: '#adc1eb',
+        marginTop: '20px',
+    },
+    service: {
+        marginTop: '20px',
+        marginBottom: '10px',
     },
     avatar: {
         margin: 20,
@@ -90,17 +98,25 @@ const style = theme => ({
         marginLeft: '40px',
     },
     paper: {
-        widht: '90%',
+        widht: '100%',
         height: 'auto',
         marginTop: '20px',
         marginLeft: '10px',
     },
+    zoomOutProperties: {
+        duration: 5000,
+        transitionDuration: 500,
+        infinite: true,
+        indicators: true,
+        scale: 0.4,
+        arrows: true
+    }
 })
 class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            items: [],
         }
     }
     // để chạy db.json/database :  json-server --watch db.json --port 3333
@@ -169,7 +185,7 @@ class Home extends Component {
                         Mỹ Phẩm
                     </p>
                     <Row>
-                        <Col md={6} xs={6}>
+                        <Col md={6} xs={{size: 12}}>
                             <Paper className={classes.paper}>
                                 <Col xs={12} md={12} style={{ textAlign: 'center', backgroundColor: '#F57249', padding: '10px' }}>
                                     Giao dich gần đây
@@ -181,7 +197,6 @@ class Home extends Component {
                                                 <TableCell align="left">Tên sản phẩm</TableCell>
                                                 <TableCell align="left">Số Lượng</TableCell>
                                                 <TableCell align="left">Đơn giá(vnđ)</TableCell>
-                                                <TableCell align="left">Tổng(vnđ)</TableCell>
                                             </TableRow>
                                         </TableHead>
                                         <TableBody>
@@ -189,23 +204,30 @@ class Home extends Component {
                                                 <TableCell>SUM Time ENERGY</TableCell>
                                                 <TableCell>2</TableCell>
                                                 <TableCell>250.000</TableCell>
-                                                <TableCell>500.000</TableCell>
                                             </TableRow>
                                             <TableRow>
                                                 <TableCell>Nước hoa hồng laneige</TableCell>
                                                 <TableCell>3</TableCell>
                                                 <TableCell>250.000</TableCell>
-                                                <TableCell>750.000</TableCell>
                                             </TableRow>
                                         </TableBody>
                                     </Table>
                                 </Col>
                             </Paper>
                         </Col>
-                        <Col md={6} xs={6}>
+                        <Col md={6} xs={{size: 12}}>
                             <Paper className={classes.paper}>
                                 <Col xs={12} md={12} style={{ textAlign: 'center', backgroundColor: '#F57249', padding: '10px' }}>
                                     Những sản phẩm mới gần đây
+                                </Col>
+                                <Col xs={12} md={12}>
+                                    <Zoom className={classes.zoomOutProperties}>
+                                        {
+                                            this.state.items.slice(0, this.state.visible).map((e, index) => (
+                                                <img key={index} src={e.url} style={{ width: '100%', height: '400px' }} />
+                                            ))
+                                        }
+                                    </Zoom>
                                 </Col>
                             </Paper>
                         </Col>
@@ -219,7 +241,81 @@ class Home extends Component {
                     >
                         Thực phẩm chức năng
                     </p>
+                    <Row>
+                        <Col md={6} xs={{size: 12}}>
+                            <Paper className={classes.paper}>
+                                <Col xs={12} md={12} style={{ textAlign: 'center', backgroundColor: '#F57249', padding: '10px' }}>
+                                    Những sản phẩm mới gần đây
+                                </Col>
+                                <Col xs={12} md={12}>
+                                    <Zoom className={classes.zoomOutProperties}>
+                                        {
+                                            this.state.items.slice(0, this.state.visible).map((e, index) => (
+                                                <img key={index} src={e.url} style={{ width: '100%', height: '400px' }} />
+                                            ))
+                                        }
+                                    </Zoom>
+                                </Col>
+                            </Paper>
+                        </Col>
+                        <Col md={6} xs={{size: 12}}>
+                            <Paper className={classes.paper}>
+                                <Col xs={12} md={12} style={{ textAlign: 'center', backgroundColor: '#F57249', padding: '10px' }}>
+                                    Giao dich gần đây
+                               </Col>
+                                <Col xs={12} md={12}>
+                                    <Table>
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell align="left">Tên sản phẩm</TableCell>
+                                                <TableCell align="left">Số Lượng</TableCell>
+                                                <TableCell align="left">Đơn giá(vnđ)</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            <TableRow>
+                                                <TableCell>SUM Time ENERGY</TableCell>
+                                                <TableCell>2</TableCell>
+                                                <TableCell>250.000</TableCell>
+                                            </TableRow>
+                                            <TableRow>
+                                                <TableCell>Nước hoa hồng laneige</TableCell>
+                                                <TableCell>3</TableCell>
+                                                <TableCell>250.000</TableCell>
+                                            </TableRow>
+                                        </TableBody>
+                                    </Table>
+                                </Col>
+                            </Paper>
+                        </Col>
+                    </Row>
                 </Col>
+               
+                <Col md={{size: 12}} xs={{size: 12}} className={classes.footer}>
+                <Container>
+                    <Row>
+                        <Col md={4} xs={{size: 12}}>
+                            <h2 className={classes.service}>Thống Kê</h2>
+                            <p style={{border: '1px solid black'}}></p>
+                            <p>Tổng số sản phẩm bán ra : </p>
+                            <p style={{marginBottom: '20px'}}>Số sản phẩm đang còn: </p>
+                        </Col>
+                        <Col md={4} xs={{size: 12}}>
+                            <h2  className={classes.service} >Liên hệ</h2>
+                            <p style={{border: '1px solid black'}}></p>
+                            <p>FaceBook: Đỗ Tiến Dương</p>
+                            <p style={{marginBottom: '20px'}}>SĐT: 0326609183</p>
+                        </Col>
+                        <Col md={4} xs={{size: 12}}>
+                            <h2  className={classes.service} >Dịch vụ khác</h2>
+                            <p style={{border: '1px solid black'}}></p>
+                            <p></p>
+                            <p style={{marginBottom: '20px'}}></p>
+                        </Col>
+                    </Row>
+                    </Container>
+                </Col>
+                
             </div>
         )
     }
