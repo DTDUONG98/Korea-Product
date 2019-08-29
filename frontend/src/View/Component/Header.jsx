bb259e1692f3bff57f9c16184f4ec266b1d59ae1
dotienduong <do.tien.duong@mqsolutions.com.vn>
2019-08-29 12:01:34 +0700

    update header

--- frontend/src/View/Component/Header.jsx
+++ frontend/src/View/Component/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
+import { Col, Row } from 'reactstrap'
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -16,7 +17,10 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import MailIcon from '@material-ui/icons/Mail'
+import { FaSistrix } from "react-icons/fa"
+import InputBase from '@material-ui/core/InputBase'
+import { fade } from '@material-ui/core/styles'
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
@@ -35,11 +39,6 @@ const useStyles = makeStyles(theme => ({
         position: 'absolute !important',
         right: '0 !important',
         margin: '0px 24px !important',
-    },
-    fixListStyle: {
-        listStyleType: 'none',
-        float: 'left',
-        marginRight: '10px'
     },
     appBar: {
         transition: theme.transitions.create(['margin', 'width'], {
@@ -61,7 +60,6 @@ const useStyles = makeStyles(theme => ({
     },
     menuButton: {
         marginRight: theme.spacing(2),
-
     },
     hide: {
         display: 'none',
@@ -95,6 +93,43 @@ const useStyles = makeStyles(theme => ({
             duration: theme.transitions.duration.enteringScreen,
         }),
         marginLeft: 0,
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        width: theme.spacing(7),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: 120,
+            '&:focus': {
+                width: 200,
+            },
+        },
     },
 }));
 
@@ -159,6 +194,35 @@ export default function PersistentDrawerLeft(props) {
                             HELTHYFOOD
                         </a>
                     </Typography>
+                    <Col md={{ size: 2, offset: 5 }}>
+                        <Typography>
+                            <div className={classes.search}>
+                                <div className={classes.searchIcon}>
+                                    <FaSistrix style={{ fontSize: '25px' }} />
+                                </div>
+                                <InputBase placeholder="Search…" classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }} inputProps={{ 'aria-label': 'Search' }} />
+                            </div>
+                        </Typography>
+                    </Col>
+                    <Col md="5">
+                        <Row>
+                            <Col md="2" >
+                                {/* <Avatar alt="PTIT" src={img} className={classes.avatar} /> */}
+                                <Typography onClick={Signin}>
+                                    Sign In
+                               </Typography>
+                            </Col>
+                            <Col md="3" >
+                                {/* <Avatar alt="PTIT" src={img} className={classes.avatar} /> */}
+                                <Typography onClick={SignUp}>
+                                    Sign Up
+                                </Typography>
+                            </Col>
+                        </Row>
+                    </Col>
                 </Toolbar>
             </AppBar>
             <Drawer
