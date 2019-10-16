5304571b8b892f6df749502a14b76e8cfb3ef59d
DTDUONG98 <dotienduongptit@gmail.com>
2019-10-16 21:23:44 +0700

    update list order ( thêm lịch và format ngày giờ)

--- frontend/src/View/Order/ListOrder.jsx
+++ frontend/src/View/Order/ListOrder.jsx
@@ -11,6 +11,8 @@ import { Col, Row } from 'reactstrap'
 import Print from '@material-ui/icons/Print'
 import { IconButton, Tooltip } from '@material-ui/core'
 import TableToExcel from "@linways/table-to-excel"
+import Calendar from 'react-calendar'
+import moment from 'moment'
 const styles = theme => ({
     root: {
         width: '100%',
@@ -26,11 +28,51 @@ const styles = theme => ({
         fontSize: '30px'
     }
 })
+const columns = [
+    { id: 'STT', label: 'STT', minWidth: 70 },
+    { id: 'product', label: 'Tên mặt hàng', minWidth: 170 },
+    { id: 'userName', label: 'Tên khách hàng', minWidth: 170 },
+    {
+        id: 'phone',
+        label: 'Số điện thoại',
+        minWidth: 170,
+        align: 'left'
+    },
+    {
+        id: 'quantity',
+        label: 'Số lượng',
+        minWidth: 100,
+        align: 'left',
+        format: value => value.toLocaleString(),
+    },
+    {
+        id: 'cost',
+        label: 'Tổng giá (vnd)',
+        minWidth: 170,
+        align: 'left',
+        format: value => value.toLocaleString(),
+    },
+    {
+        id: 'date',
+        label: 'Ngày đặt hàng',
+        minWidth: 170,
+        align: 'left',
+    },
+    {
+        id: 'address',
+        label: 'Đại chỉ nhận hàng',
+        minWidth: 270,
+        align: 'left',
+    },
+];
+
 class ListOrder extends Component {
     constructor(props) {
         super(props);
         this.state = {
             items: [],
+            date: new Date(),
+            DateSelect: '',
         }
     }
     componentDidMount() {
@@ -41,19 +83,27 @@ class ListOrder extends Component {
             )
             .catch(error => console.log('error ', error));
     }
-    printTblToEx(){
+    printTblToEx() {
         let table = document.querySelector('table')
         TableToExcel.convert(table)
+    }
+    onChange = (event) => {
+        let date = moment(event).format('YYYY-MM-DD')
+        console.log('date', date)
+        this.setState({
+            date: event,
+            DateSelect: date,
+        })
     }
     render() {
         const { classes } = this.props
         return (
             <div>
-                <Header 
-                    link = {this.props}
+                <Header
+                    link={this.props}
                 />
                 <Col xs="12" md="12">
-                    <h2 style={{textAlign: "center"}}>Danh sách Order</h2>
+                    <h2 style={{ textAlign: "center" }}>Danh sách Order</h2>
                 </Col>
                 <Col xs={{ size: 1, offset: 11 }} md={{ size: 1, offset: 11 }}>
                     <Tooltip title="Print to Excel" key="Print">
@@ -62,35 +112,46 @@ class ListOrder extends Component {
                         </IconButton>
                     </Tooltip>
                 </Col>
+                <Col xs="12" md="12">
+                    <Calendar
+                        onChange={this.onChange}
+                        value={this.state.date}
+                    />
+                </Col>
                 <Table className={classes.table} data-cols-width="5,30,30,15,10,20,20,40">
                     <TableHead>
                         <TableRow>
-                            <TableCell align="left">STT</TableCell>
-                            <TableCell align="left">Tên Mặt Hàng</TableCell>
-                            <TableCell align="left">Tên Khách Hàng</TableCell>
-                            <TableCell align="left">Số Điện Thoại</TableCell>
-                            <TableCell align="left">Số lượng sản phẩm</TableCell>
-                            <TableCell align="left">Giá ( tổng )</TableCell>
-                            <TableCell align="left">Ngày Đặt Hàng</TableCell>
-                            <TableCell align="left">Địa chỉ nhận hàng</TableCell>
+                            {columns.map(column => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{ minWidth: column.minWidth }}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {this.state.items.map((item, index) => (
-                            <TableRow key={index}>
-                                <TableCell component="th" scope="row">{item.id}</TableCell>
-                                <TableCell align="left">Chưa lấy được</TableCell>
-                                <TableCell align="left">{item.userName}</TableCell>
-                                <TableCell align="left">{item.phone}</TableCell>
-                                <TableCell align="left">{item.quantity}</TableCell>
-                                <TableCell align="left">Chưa lấy được</TableCell>
-                                <TableCell align="left">{item.date}</TableCell>
-                                <TableCell align="left">{item.address}</TableCell>
-                            </TableRow>
-                        ))}
+                        {this.state.items.map((item, index) => {
+                            if (item.date == this.state.DateSelect) {
+                                return (
+                                    <TableRow key={index}>
+                                        <TableCell component="th" scope="row">{item.id}</TableCell>
+                                        <TableCell align="left">{item.product}</TableCell>
+                                        <TableCell align="left">{item.userName}</TableCell>
+                                        <TableCell align="left">{item.phone}</TableCell>
+                                        <TableCell align="left">{item.quantity}</TableCell>
+                                        <TableCell align="left">{item.price}</TableCell>
+                                        <TableCell align="left">{item.date}</TableCell>
+                                        <TableCell align="left">{item.address}</TableCell>
+                                    </TableRow>
+                                )
+                            }
+                        })}
                     </TableBody>
                 </Table>
-                <Footer 
+                <Footer
                     link={this.props}
                 />
             </div>
